e456e87b49de7b0fff7967ecd9d5b17f4571559c
Harsh Hatej <97379847+harshhatej85@users.noreply.github.com>
2025-02-05 16:27:16 +0530

    Update find_sensitive_files.sh

--- find_sensitive_files.sh
+++ find_sensitive_files.sh
@@ -1,6 +1,3 @@
-#finds all these different sensitive files in linux file system just give it executable permission and run as root owner.
-#You can also add more files in the "files" array. 
-
 #!/bin/bash
 
 # array of file names to search for
@@ -113,5 +110,3 @@ for file in "${files[@]}"; do
 done
 
 echo -e "\n\033[1;32m Search completed. \033[0m"
-
-
